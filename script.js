5ff8dce3d3342350fc519ea1f9f592ad79f0262e
Bruna Pessoa <brunapessoa.bpm@gmail.com>
2026-01-14 18:27:43 +0100

    creating exercises, put on the list and sending to localstorage

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ for (let index = 0; index < addButton.length; index ++) {
 
 function addExercise(event) {
     let workoutId = event.target.parentElement.id;
+    // capturando o id do elemento pai do botão clicado
     let workoutIndex = 0;
     if (workoutId == 'workoutA') {
         workoutIndex = 0;
@@ -21,44 +22,68 @@ function addExercise(event) {
         workoutIndex = 2;
     }
 
-    let divWorkout = event.target.parentElement;  //pai do elemento clicado - botao clicado 
+    let divWorkout = event.target.parentElement;  
+    //pai do elemento clicado - botao clicado 
 
     let tableElement = divWorkout.firstElementChild;
+    // acessando a tabela correspondente ao botão clicado
     
     let tbodyElement = tableElement.lastElementChild; // acessando o tbody
 
-    let exercisesList = inputExercise[workoutIndex].value; 
-    let weightsList = inputWeight[workoutIndex].value;
-    let repsList = inputReps[workoutIndex].value;
+    let exercises = inputExercise[workoutIndex].value; 
+    let weights = inputWeight[workoutIndex].value;
+    let reps = inputReps[workoutIndex].value;
     // recebem o index indicado no início da funcao, para povoar a tabela correta
 
-    let exercise = exercisesList;
-    let weight = weightsList;
-    let reps = repsList;
+    let exerciseItem = exercises;
+    let weightItem = weights;
+    let repsItem = reps;
+    let editBtn = '<button>Edit</button>'; 
+    let removeBtn = '<button class="delButton">Remove</button>';
 
-    let exerciseItem = document.createElement('tr');
-        exerciseItem.id = 'trElement';
-        exerciseItem.innerHTML = `<td>${exercise}</td><td>${weight}</td><td>${reps}</td><td><button class='delButton'>remover</button></td>`;
-        console.log(exerciseItem);      
 
-        tbodyElement.appendChild(exerciseItem);
+    let exerciseElement = document.createElement('tr');
+        exerciseElement.id = 'trElement';
+        exerciseElement.innerHTML = `<td>${exerciseItem}</td><td>${weightItem}</td><td>${repsItem}</td><td>${editBtn}</td><td>${removeBtn}</td>`;
+        console.log(exerciseElement);      
+
+        tbodyElement.appendChild(exerciseElement);
         
         inputExercise[workoutIndex].value = '';
         inputWeight[workoutIndex].value = '';
         inputReps[workoutIndex].value = '';
         
+        let exercisesList = JSON.parse(localStorage.getItem(workoutId)) ||[];
+
+        const newExercise = {
+            exercise: exerciseItem,
+            wheigths: weightItem,
+            reps: repsItem
+        }
+        exercisesList.push(newExercise);
+
+        console.log(exercisesList);
+        
+        localStorage.setItem(workoutId, JSON.stringify(exercisesList));
+
+
+
+
+        
+
         // Delete Button:
         let delButton = document.getElementsByClassName('delButton');
         
         if (delButton.length > 0) {
             
-            let butItem;
+            let removeBtn;
             for(let index = 0; index < delButton.length; index++) {
                 // adiciona a funcao delExercise em todos os botões que forem criados
-                butItem = delButton[index];
+                removeBtn = delButton[index];
             }          
-            butItem.addEventListener('click', delExercise);
+            removeBtn.addEventListener('click', delExercise);
         }
+        
         function delExercise(event) {
             let butElement = event.target.parentElement;
             // o event.target já é o elemento do botão que foi clicado
@@ -71,8 +96,8 @@ function addExercise(event) {
             // acessando o tbody, que é pai do tr
             
             tbodyElement.removeChild(trElement);
+            localStorage.removeItem(newExercise);
         }
-
 }
 
     
